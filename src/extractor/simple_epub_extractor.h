7ffbf44348b9d5df867610293e0b02852c0b5880
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2024-12-25 17:22:42 +0000

    Remove uneccesary code

--- src/extractor/simple_epub_extractor.h
+++
@@ -1,3 +0,0 @@
-#include "cJSON.h"
-
-cJSON *generate_change_request_epub(const char *file_path, const char *uuid);
--- src/extractor/simple_fb2_extractor.h
+++
@@ -1,3 +0,0 @@
-#include "cJSON.h"
-
-cJSON *generate_change_request_fb2(const char *file_path, const char *uuid);
