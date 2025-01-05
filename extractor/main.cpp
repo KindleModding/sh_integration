9261ac68861abb3871692d01cecadaeb1e3013c3
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-01-05 20:25:59 +0000

    Revert "Implement .sdr folder in extractor"
    This reverts commit 7562155cb633e6f6b223c0461aea61e47870939f.

--- extractor/main.cpp
+++ extractor/main.cpp
@@ -9,6 +9,108 @@
 #include <unistd.h>
 #include "cJSON.h"
 
+cJSON* generateChangeRequest(std::filesystem::path& filePath, const char* uuid) {
+    syslog(LOG_INFO, "Generating change request for %s\n", filePath.c_str());
+
+    std::string name_string;
+    std::string author_string;
+    std::string icon_string;
+
+    std::string line;
+
+    bool isFunctional = false;
+
+    // Read data from file header
+    std::ifstream file(filePath);
+    if (file.is_open()) {
+        for (int i=0; i < 6; i++) {
+            if (!std::getline(file, line)) {
+                break;
+            }
+
+            // Start reading the header
+            if (line.substr(0, 8) == "# Name: ") {
+                name_string = line.substr(8);
+            } else if (line.substr(0, 10) == "# Author: ") {
+                author_string = line.substr(10);
+            } else if (line.substr(0, 8) == "# Icon: ") {
+                icon_string = line.substr(8);
+            } else if (line.substr(0, 12) == "# Functional") {
+                isFunctional = true;
+            }
+        }
+        file.close(); // We are done reading the file
+    }
+
+    // If the file is functional, run install hook
+    if (isFunctional) {
+        std::string escapedPath = filePath.string();
+        for (int i=0; i < escapedPath.length(); i++) {
+            if (escapedPath[i] == '"') {
+                escapedPath.insert(i, "\\");
+                i += 1; // Skip character
+            }
+        }
+
+        syslog(LOG_INFO, "Running install event");
+        const int pid = fork();
+        if (pid == 0) {
+            syslog(LOG_INFO, "Executing command: %s", ("source \"" + escapedPath + "\"; on_install;").c_str());
+            execl("/var/local/mkk/su", "su", "-c", ("source \"" + escapedPath + "\"; on_install;").c_str(), NULL);
+        } else {
+            waitpid(pid, NULL, 0);
+        }
+    }
+
+    struct stat st;
+    stat(filePath.c_str(), &st);
+
+    cJSON* json = cJSON_CreateObject();
+    cJSON_AddItemToObject(json, "uuid", cJSON_CreateString(uuid));
+    cJSON_AddItemToObject(json, "location", cJSON_CreateString(filePath.c_str()));
+    cJSON_AddItemToObject(json, "type", cJSON_CreateString("Entry:Item"));
+    cJSON_AddItemToObject(json, "cdeType", cJSON_CreateString("PDOC"));
+    cJSON_AddItemToObject(json, "cdeKey", cJSON_CreateString(getSha1Hash(filePath.c_str())));
+    cJSON_AddItemToObject(json, "modificationTime", cJSON_CreateNumber(st.st_mtim.tv_sec));
+    cJSON_AddItemToObject(json, "diskUsage", cJSON_CreateNumber(st.st_size));
+    cJSON_AddItemToObject(json, "isVisibleInHome", cJSON_CreateTrue());
+    cJSON_AddItemToObject(json, "isArchived", cJSON_CreateFalse());
+    cJSON_AddItemToObject(json, "mimeType", cJSON_CreateString("text/x-shellscript"));
+    cJSON *authors = cJSON_CreateArray();
+    cJSON *author = cJSON_CreateObject();
+    cJSON_AddItemToObject(author, "kind", cJSON_CreateString("Author"));
+    cJSON *author_name = cJSON_CreateObject();
+    cJSON_AddItemToObject(author_name, "display", cJSON_CreateString((const char *)(author_string.length() > 0 ? author_string.c_str() : "Unknown")));
+    cJSON_AddItemToObject(author, "name", author_name);
+    cJSON_AddItemToArray(authors, author);
+    cJSON_AddItemToObject(json, "credits", authors);
+    cJSON_AddItemToObject(json, "publisher", cJSON_CreateString("KMC"));
+    cJSON *refs = cJSON_CreateArray();
+    cJSON *titles_ref = cJSON_CreateObject();
+    cJSON_AddItemToObject(titles_ref, "ref", cJSON_CreateString("titles"));
+    cJSON_AddItemToArray(refs, titles_ref);
+    cJSON *authors_ref = cJSON_CreateObject();
+    cJSON_AddItemToObject(authors_ref, "ref", cJSON_CreateString("credits"));
+    cJSON_AddItemToArray(refs, authors_ref);
+    cJSON_AddItemToObject(json, "displayObjects", refs);
+    const char *tags[] = {"NEW"};
+    cJSON_AddItemToObject(json, "displayTags", cJSON_CreateStringArray(tags, 1));
+    if (icon_string.length() > 0) {
+        cJSON_AddItemToObject(json, "thumbnail",
+                            cJSON_CreateString(icon_string.c_str()));
+    }
+    cJSON *titles = cJSON_CreateArray();
+    cJSON *title = cJSON_CreateObject();
+    cJSON_AddItemToObject(
+        title, "display",
+        cJSON_CreateString((const char *)(name_string.length() > 0 ? name_string.c_str() : filePath.filename().c_str())));
+    cJSON_AddItemToArray(titles, title);
+    cJSON_AddItemToObject(json, "titles", titles);
+
+    return json;
+}
+
+typedef cJSON* (ChangeRequestGenerator)(const char*& file_path, const char*& uuid);
 void index_file(char *path, char* filename) {
     syslog(LOG_INFO, "Indexing file: %s/%s", path, filename);
 
@@ -45,111 +147,7 @@ void index_file(char *path, char* filename) {
         return;
     }
 
-    /**
-     * Generate the (huge) change request JSON
-     * 
-     */
-    syslog(LOG_INFO, "Generating change request for %s\n", full_path.c_str());
-    std::string name_string;
-    std::string author_string;
-    std::string icon_string;
-
-    std::string line;
-
-    bool useHooks = false;
-
-    // Read data from file header
-    std::ifstream file(full_path);
-    if (file.is_open()) {
-        for (int i=0; i < 6; i++) {
-            if (!std::getline(file, line)) {
-                break;
-            }
-
-            // Start reading the header
-            if (line.substr(0, 8) == "# Name: ") {
-                name_string = line.substr(8);
-            } else if (line.substr(0, 10) == "# Author: ") {
-                author_string = line.substr(10);
-            } else if (line.substr(0, 8) == "# Icon: ") {
-                icon_string = line.substr(8);
-            } else if (line.substr(0, 10) == "# UseHooks") {
-                useHooks = true;
-            }
-        }
-        file.close(); // We are done reading the file
-    }
-
-    // If the file uses hooks, run install hook
-    if (useHooks) {
-        std::string escapedPath = full_path.string();
-        for (int i=0; i < escapedPath.length(); i++) {
-            if (escapedPath[i] == '"') {
-                escapedPath.insert(i, "\\");
-                i += 1; // Skip character
-            }
-        }
-
-        syslog(LOG_INFO, "Running install event");
-        const int pid = fork();
-        if (pid == 0) {
-            syslog(LOG_INFO, "Executing command: %s", ("source \"" + escapedPath + "\"; on_install;").c_str());
-            execl("/var/local/mkk/su", "su", "-c", ("source \"" + escapedPath + "\"; on_install;").c_str(), NULL);
-        } else {
-            waitpid(pid, NULL, 0);
-        }
-
-        syslog(LOG_INFO, "Creating sdr folder...");
-        std::string sdr_path = full_path.string() + ".sdr";
-        std::filesystem::remove_all(sdr_path); // Delete previous sdr folder
-        std::filesystem::create_directory(sdr_path); // Create the sdr folder
-        std::filesystem::copy(full_path, sdr_path + "/uninstall.sh");
-    }
-
-    struct stat st;
-    stat(full_path.c_str(), &st);
-
-    cJSON* change = cJSON_CreateObject();
-    cJSON_AddItemToObject(json, "uuid", cJSON_CreateString(uuid));
-    cJSON_AddItemToObject(json, "location", cJSON_CreateString(full_path.c_str()));
-    cJSON_AddItemToObject(json, "type", cJSON_CreateString("Entry:Item"));
-    cJSON_AddItemToObject(json, "cdeType", cJSON_CreateString("PDOC"));
-    cJSON_AddItemToObject(json, "cdeKey", cJSON_CreateString(getSha1Hash(full_path.c_str())));
-    cJSON_AddItemToObject(json, "modificationTime", cJSON_CreateNumber(st.st_mtim.tv_sec));
-    cJSON_AddItemToObject(json, "diskUsage", cJSON_CreateNumber(st.st_size));
-    cJSON_AddItemToObject(json, "isVisibleInHome", cJSON_CreateTrue());
-    cJSON_AddItemToObject(json, "isArchived", cJSON_CreateFalse());
-    cJSON_AddItemToObject(json, "mimeType", cJSON_CreateString("text/x-shellscript"));
-    cJSON *authors = cJSON_CreateArray();
-    cJSON *author = cJSON_CreateObject();
-    cJSON_AddItemToObject(author, "kind", cJSON_CreateString("Author"));
-    cJSON *author_name = cJSON_CreateObject();
-    cJSON_AddItemToObject(author_name, "display", cJSON_CreateString((const char *)(author_string.length() > 0 ? author_string.c_str() : "Unknown")));
-    cJSON_AddItemToObject(author, "name", author_name);
-    cJSON_AddItemToArray(authors, author);
-    cJSON_AddItemToObject(json, "credits", authors);
-    cJSON_AddItemToObject(json, "publisher", cJSON_CreateString("KMC"));
-    cJSON *refs = cJSON_CreateArray();
-    cJSON *titles_ref = cJSON_CreateObject();
-    cJSON_AddItemToObject(titles_ref, "ref", cJSON_CreateString("titles"));
-    cJSON_AddItemToArray(refs, titles_ref);
-    cJSON *authors_ref = cJSON_CreateObject();
-    cJSON_AddItemToObject(authors_ref, "ref", cJSON_CreateString("credits"));
-    cJSON_AddItemToArray(refs, authors_ref);
-    cJSON_AddItemToObject(json, "displayObjects", refs);
-    const char *tags[] = {"NEW"};
-    cJSON_AddItemToObject(json, "displayTags", cJSON_CreateStringArray(tags, 1));
-    if (icon_string.length() > 0) {
-        cJSON_AddItemToObject(json, "thumbnail",
-                            cJSON_CreateString(icon_string.c_str()));
-    }
-    cJSON *titles = cJSON_CreateArray();
-    cJSON *title = cJSON_CreateObject();
-    cJSON_AddItemToObject(
-        title, "display",
-        cJSON_CreateString((const char *)(name_string.length() > 0 ? name_string.c_str() : full_path.filename().c_str())));
-    cJSON_AddItemToArray(titles, title);
-    cJSON_AddItemToObject(json, "titles", titles);
+    cJSON* change = generateChangeRequest(full_path, uuid);
     if (!change) {
         syslog(LOG_CRIT, "Failed to generate a change request...");
         cJSON_Delete(json);
@@ -213,13 +211,11 @@ void remove_file(const char* path, const char* filename, char* uuid) {
     filePath += path;
     filePath += "/";
     filePath += filename;
-
-    std::string sdr_path = filePath.string() + ".sdr";
+    bool isFunctional = false;
 
     // Read data from file header
-    bool useHooks = false;
     std::string line;
-    std::ifstream file(sdr_path + "/uninstall.sh"); // We use an sdr folder to store a copy of the script as the file is deleted before remove_file is called
+    std::ifstream file(filePath);
     if (file.is_open()) {
         for (int i=0; i < 6; i++) {
             if (!std::getline(file, line)) {
@@ -227,17 +223,17 @@ void remove_file(const char* path, const char* filename, char* uuid) {
             }
             
             // Look for Functional flag
-            if (line.substr(0, 10) == "# UseHooks") {
-                useHooks = true;
+            if (line.substr(0, 12) == "# Functional") {
+                isFunctional = true;
             }
         }
         file.close(); // We are done reading the file
     } else {
-        syslog(LOG_INFO, "Unable to open file %s", (sdr_path + "/uninstall.sh").c_str());
+        syslog(LOG_INFO, "Unable to open file %s", filePath.string().c_str());
     }
 
     // If the file is functional, run install hook
-    if (useHooks) {
+    if (isFunctional) {
         std::string escapedPath = filePath.string();
         for (int i=0; i < escapedPath.length(); i++) {
             if (escapedPath[i] == '"') {
@@ -257,8 +253,7 @@ void remove_file(const char* path, const char* filename, char* uuid) {
     }
 
     // Actually remove the file (and the entry)
-    //std::filesystem::remove(filePath); // This is handled externally, actually
-    std::filesystem::remove_all(sdr_path); // Delete sdr folder
+    std::filesystem::remove(filePath);
     scanner_delete_ccat_entry(uuid);
 }
 
