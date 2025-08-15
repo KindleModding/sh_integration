981b3b5262d8affddc6be08a25d3fce150e63c59
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-08-15 18:08:52 +0100

    More scripts for testing

--- tests/launcher_test_generate_command.c
+++ tests/launcher_test_generate_command.c
@@ -1,8 +1,12 @@
 #include "launcher.h"
+#include <assert.h>
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    printf("tester");
+    char* command = getScriptCommand("./test.sh");
+    assert(strcmp(command, "sh -c \"source \\\"./test.sh\\\"; on_run;\"") == 0);
+    
     return 0;
 }
